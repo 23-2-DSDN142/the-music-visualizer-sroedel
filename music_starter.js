b7186129c0caa7ac7bfc3db7f8ff81c94ed93f32
sroedel <sproedel14@gmail.com>
2023-10-05 10:35:27 +1300

    messing around

--- music_starter.js
+++ music_starter.js
@@ -11,40 +11,43 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   angleMode(DEGREES);
   colorMode(HSB,100);
   background("black");
-  strokeWeight(9);
+  strokeWeight(1);
   stroke (drum,850,80)
 
 
   colorMode(RGB, 255)
 
-let yellowColor = color(237, 170, 0)
-let blueColor = color(9, 20, 135)
+let yellowColor = color(75, 66, 245);
+let blueColor = color(226, 56, 245);
 
 let colorChange = map(vocal, 0,100, 0,1)
 let blendedColor = lerpColor(yellowColor,blueColor, colorChange)
 
-fill(blendedColor)
-ellipse(200,200,200,200)
+colorMode(HSB,100);
 
-  let drumMap = map(drum,0,100,40,100);
-  let lengthOfLine= 100;
-  let lineStart = 0;
-  let lineEnd = lineStart+lengthOfLine;
+//circle
+
+  fill("black");
+  if(song.currentTime() > 62){
+    fill(drum,850,80);
+    }
+  ellipse(500,500,400,400)
+
   
+  // //Dots
+  // colorMode(RGB, 255)
+  // fill(blendedColor)
+  // strokeWeight(1);
+  // let bassmap =map(bass,0,100,40,100);
+  // let ballsize = 10 ;
+  // let ballx = 150;
+  // let bally =0;
   
-  
-  
-  strokeWeight(1);
-  let bassmap =map(bass,0,100,40,100);
-  let ballsize = 10 ;
-  let ballx = 150;
-  let bally =0;
-  
-  
-  for(let i= 1; i<= bassmap; i++){
-    let ballStep =i*11;
-    ellipse (ballx,ballStep,ballsize)
-  }
+  // colorMode(HSB,100);
+  // for(let i= 1; i<= bassmap; i++){
+  //   let ballStep =i*11;
+  //   ellipse (ballx,ballStep,ballsize)
+  // }
    
 
 
@@ -52,8 +55,8 @@ ellipse(200,200,200,200)
 
 
 
-
-
+//rectangle
+fill(blendedColor);
 translate(x,y);
 rotate(angle);
 rect(0,0,100,50);
